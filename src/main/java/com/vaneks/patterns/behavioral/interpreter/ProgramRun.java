f530fffdbee83405972911c38a4549aec058d971
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-24 00:12:16 +0800

    Add files via upload

--- src/main/java/com/vaneks/patterns/behavioral/interpreter/ProgramRun.java
+++ src/main/java/com/vaneks/patterns/behavioral/interpreter/ProgramRun.java
@@ -2,19 +2,19 @@ package main.java.com.vaneks.patterns.behavioral.interpreter;
 
 public class ProgramRun {
     public static void main(String[] args) {
-        Expression isJava = getJavaExpression();
-        Expression isJavaEE = getJavaEEExpression();
-        System.out.println("This developers knows Java Core: " + isJava.interpret("Java Core"));
-        System.out.println("This developers knows Java EE: " + isJavaEE.interpret("Java EE"));
+        Expression isHumanitarian = getHumanitarianExpression();
+        Expression isNatural = getNaturalExpression();
+        System.out.println("This student knows humanitarian sciences: " + isHumanitarian.interpret("English"));
+        System.out.println("This student knows natural sciences: " + isNatural.interpret("Мaths and Physics"));
     }
-    public static Expression getJavaExpression() {
-        Expression java = new TerminalExpression("Java");
-        Expression javaCore = new TerminalExpression("Java Core");
-        return new OrExpression(java, javaCore);
+    public static Expression getHumanitarianExpression() {
+        Expression history = new TerminalExpression("History");
+        Expression javaCore = new TerminalExpression("English");
+        return new OrExpression(history, javaCore);
     }
-    public static Expression getJavaEEExpression() {
-        Expression java = new TerminalExpression("Java");
-        Expression javaEE = new TerminalExpression("Java EE");
+    public static Expression getNaturalExpression() {
+        Expression java = new TerminalExpression("Physics");
+        Expression javaEE = new TerminalExpression("Мaths");
         return new AndExpression(java, javaEE);
     }
 
