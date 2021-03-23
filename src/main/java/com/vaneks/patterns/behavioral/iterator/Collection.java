524442edb33ff976b6f151e568fbf3b502fcfb6f
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-23 23:44:54 +0800

    Add files via upload

---
+++ src/main/java/com/vaneks/patterns/behavioral/iterator/Collection.java
@@ -0,0 +1,5 @@
+package main.java.com.vaneks.patterns.behavioral.iterator;
+
+public interface Collection {
+    Iterator getIterator();
+}
---
+++ src/main/java/com/vaneks/patterns/behavioral/iterator/Iterator.java
@@ -0,0 +1,6 @@
+package main.java.com.vaneks.patterns.behavioral.iterator;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
