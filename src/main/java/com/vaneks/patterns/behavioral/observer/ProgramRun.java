b67fd927188da8f2a1a2cddaa2e078aa83d8f3a5
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-25 17:44:42 +0800

    Add files via upload

---
+++ src/main/java/com/vaneks/patterns/behavioral/observer/ProgramRun.java
@@ -0,0 +1,19 @@
+package main.java.com.vaneks.behavioral.observer;
+
+import javax.management.ObjectName;
+
+public class ProgramRun {
+    public static void main(String[] args) {
+        OnlineStore onlineStore = new OnlineStore();
+        onlineStore.addProducts("Monitor");
+        onlineStore.addProducts("KeyBoard");
+
+        Observer customer1 = new Customers("Nick");
+        Observer customer2 = new Customers("Ivan");
+
+        onlineStore.addObserver(customer1);
+        onlineStore.addObserver(customer2);
+        onlineStore.addProducts("Mouse");
+        onlineStore.removeProduct("KeyBoard");
+    }
+}
