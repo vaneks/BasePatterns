a05d088134bcb0d3b49bba77083881e1f8839b57
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-27 12:09:50 +0800

    Delete src/main/java/com/vaneks/patterns/behavioral/visitor directory

--- src/main/java/com/vaneks/patterns/behavioral/visitor/Developer.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.vaneks.behavioral.visitor;
-
-public interface Developer {
-    public void create(ProjectClass projectClass);
-    public void create(DataBase dataBase);
-    public void create(Test test);
-}
