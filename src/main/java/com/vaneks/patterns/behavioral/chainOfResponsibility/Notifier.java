1a7c85cd76b20a570d5cdf98c02fc441885a5118
Ivan <35092751+vaneks@users.noreply.github.com>
2021-03-27 14:29:09 +0800

    Add files via upload

--- src/main/java/com/vaneks/patterns/behavioral/chainOfResponsibility/Notifier.java
+++ src/main/java/com/vaneks/patterns/behavioral/chainOfResponsibility/Notifier.java
@@ -2,22 +2,22 @@ package main.java.com.vaneks.patterns.behavioral.chainOfResponsibility;
 
 public abstract class Notifier {
     private int priority;
-    private Notifier newtNotifier;
+    private Notifier newNotifier;
 
     public Notifier(int priority) {
         this.priority = priority;
     }
 
     public void setNextNotifier(Notifier nextNotifier) {
-        this.newtNotifier = newtNotifier;
+        this.newNotifier = nextNotifier;
     }
 
     public void notifyManager(String message, int level) {
         if(level >= priority) {
             write(message);
         }
-        if(newtNotifier != null) {
-            newtNotifier.notifyManager(message, level);
+        if(newNotifier != null) {
+            newNotifier.notifyManager(message, level);
         }
     }
     public abstract void write(String message);
